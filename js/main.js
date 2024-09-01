e62eedf932d39cbeee5168f7efeca94b33e42527
Amr maher <amrr.maherr24@gmail.com>
2024-09-02 00:28:33 +0300

    Add functions to fetch and display health news articles
    - Added `requestDataTwo()` function to fetch health news articles from the API.
    - Added `data()` function to process and display the fetched articles in a responsive card layout.
    - Updated HTML structure with a new container and row class for displaying articles.
    - Integrated AOS (Animate On Scroll) for zoom-in effect on card elements.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function requestData() {
+function requestDataOne() {
   let xHttp = new XMLHttpRequest();
   xHttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -10,8 +10,48 @@ function requestData() {
   xHttp.send();
 }
 
+function requestDataTwo() {
+  let xHttp = new XMLHttpRequest();
+  xHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let articles = JSON.parse(this.responseText);
+      data(articles.articles);
+    }
+  };
+  xHttp.open(
+    "GET",
+    "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json",
+    false
+  );
+  xHttp.send();
+}
+
+function data(articles) {
+  let row = document.querySelector(".rowTwo");
+  let content = "";
+  articles.forEach(function (article) {
+    content += `
+            <div class='col-md-6 col-12 my-4 '>
+                <div data-aos="zoom-in">
+                  <a href='${article.url}'>
+                  <div class="card text-bg-dark text-dark">
+  <img src="${article.urlToImage}" class="card-img h-100" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title fs-2">${article.title}</h5>
+    <p class="card-text fs-4">${article.description}.</p>
+    <p class="card-text fs-5"><small>${article.publishedAt}</small></p>
+  </div>
+</div></a>
+                </div>
+            </div>
+        `;
+  });
+  row.innerHTML = content;
+}
+
+
 function showData(articles) {
-  let row = document.querySelector(".row");
+  let row = document.querySelector(".rowOne");
   let content = "";
   articles.forEach(function (article) {
     content += `
@@ -37,6 +77,8 @@ function showData(articles) {
 }
 
 window.onload = function () {
-    requestData();
+    requestDataOne();
+    requestDataTwo();
     AOS.init();
 };
+
